e81aafa9622a0229a5045560c2bb5ae63ce03465
Paul <git@paul.sx>
2021-12-15 17:58:56 -0500

    Early draft

--- huginn_mattermost_form.gemspec
+++ huginn_mattermost_form.gemspec
@@ -11,7 +11,7 @@ Gem::Specification.new do |spec|
   spec.summary       = %q{Write a short summary, because Rubygems requires one.}
   spec.description   = %q{Write a longer description or delete this line.}
 
-  spec.homepage      = "https://github.com/[my-github-username]/huginn_mattermost_form"
+  spec.homepage      = "https://github.com/paul-sx/huginn_mattermost_form"
 
   spec.license       = "MIT"
 
--- lib/huginn_mattermost_form/mattermost_form.rb
+++ lib/huginn_mattermost_form/mattermost_form.rb
@@ -1,6 +1,12 @@
 module Agents
   class MattermostForm < Agent
-    default_schedule '12h'
+    include EventHeadersConcern
+    include WebRequestConcern
+
+    default_schedule 'never'
+    no_bulk_receive!
+    cannot_receive_events!
+    cannot_be_scheduled!
 
     description <<-MD
       Add a Agent description here
@@ -8,6 +14,31 @@ module Agents
 
     def default_options
       {
+        'secret' => 'supersecretstring',
+        'post_url' => 'https://mm.example.com/api/v4/actions/dialogs/open',
+        'expected_receive_period_in_days' => '31',
+        'payload' => {
+          'url' => 'https://huginn.example.com/users/1/web_requests/1/supersecretstring',
+          'trigger_id' => "{{ trigger_id }}",
+          'dialog' => {
+            'callback_id' => "{{ context.send_from }}",
+            'title' => "Send Reply Text",
+            'introduction_text': "Sending from {{ context.send_from }}",
+            'elements' => [
+              {
+                'display_name' => "Reply Text",
+                'name' => 'reply_text',
+                'type' => 'textarea',
+                'help_text' => "Type in your message"
+              }
+            ],
+            'submit_label' => "Send",
+            'notify_on_cancel' => false,
+            'state' => "{{ context.reply_to }}"
+          }
+
+        },
+        'headers' => {}
       }
     end
 
@@ -43,13 +74,30 @@ module Agents
 
       code = (interpolated['code'].presence || 201).to_i
 
-      # Add stuff here to get the pokemon ball
+      interpolate_with(params) do
+        post_url = interpolated['post_url']
+        post_headers = headers(interpolated[:headers])
+        post_body = interpolated['payload']
+        post_method = 'post'
+        post_headers['Content-Type'] = 'application/json; charset=utf-8'
+
+        response = faraday.run_request(post_method.to_sym, post_url, post_body, post_headers)
+
+      end
+
+
+      # payload is now ready with the data
 
       if interpolated['response_headers'].presence
-        [interpolated['response_headers']]
-
-
+        [interpolated(params)['response'] || 'Event Created', code, "text/plain", interpolated['response_headers'].presence]
+      else
+        [interpolated(params)['response'] || 'Event Created', code]
+      end
     end
+
+
+
+
 
 #    def check
 #    end
